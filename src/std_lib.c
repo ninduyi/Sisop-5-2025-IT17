d9f73bed8b7cc359081061a3876cc9fffb1dbd8c
ninduyi <anindyanabilah2@gmail.com>
2025-06-08 18:12:13 +0700

    Update std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -1,14 +1,48 @@
 #include "std_lib.h"
 
 int div(int a, int b) {
-  // TODO: Implementasi pembagian
-  // NOTE: HARUS BISA ANGKA NEGATIF
-  return 0; // Placeholder
+  int negative;
+  int result;
+  negative = 0;
+  result = 0;
+
+  if (b == 0) return 0; // Avoid division by zero
+
+  if (a < 0) {
+    a = -a;
+    negative = !negative;
+  }
+  if (b < 0) {
+    b = -b;
+    negative = !negative;
+  }
+
+  while (a >= b) {
+    a = a - b;
+    result++;
+  }
+
+  if (negative) result = -result;
+  return result;
 }
 
 int mod(int a, int b) {
-  // TODO: Implementasi modulus
-  return 0; // Placeholder
+  int negative = 0;
+
+  if (b == 0) return 0;
+
+  if (a < 0) {
+    a = -a;
+    negative = 1;
+  }
+  if (b < 0) b = -b;
+
+  while (a >= b) {
+    a = a - b;
+  }
+
+  if (negative) a = -a;
+  return a;
 }
 
 bool strcmp(char *str1, char *str2) {
@@ -22,9 +56,6 @@ bool strcmp(char *str1, char *str2) {
   return (str1[i] == '\0' && str2[i] == '\0');
 }
 
-// Implementasi penyalinan string
-// Menyalin isi string src ke dst.
-// PERHATIAN: Pastikan dst memiliki cukup ruang!
 void strcpy(char *dst, char *src) {
   int i = 0;
   while (src[i] != '\0') {
@@ -39,11 +70,55 @@ void clear(byte *buf, unsigned int size) {
 }
 
 void atoi(char *str, int *num) {
-  // TODO: Implementasi konversi string ke integer
-  // NOTE: HARUS BISA ANGKA NEGATIF
+  int i = 0;
+  int sign = 1;
+  int result = 0;
+
+  if (str[0] == '-') {
+    sign = -1;
+    i++;
+  }
+
+  while (str[i] != '\0') {
+    result = result * 10 + (str[i] - '0');
+    i++;
+  }
+
+  *num = sign * result;
 }
 
-void itoa(int num, char *str) {
+
   // TODO: Implementasi konversi integer ke string
   // NOTE: HARUS BISA ANGKA NEGATIF
+void itoa(int num, char *str) {
+  int i = 0;
+  int isNegative = 0;
+  char tempStr[20];
+  int j;
+
+  if (num == 0) {
+    str[0] = '0';
+    str[1] = '\0';
+    return;
+  }
+
+  if (num < 0) {
+    isNegative = 1;
+    num = -num;
+  }
+
+  while (num != 0) {
+    int remainder = mod(num, 10);
+    tempStr[i++] = remainder + '0';
+    num = div(num, 10);
+  }
+
+  if (isNegative) {
+    tempStr[i++] = '-';
+  }
+
+  for (j = 0; j < i; j++) {
+    str[j] = tempStr[i - j - 1];
+  }
+  str[j] = '\0';
 }
